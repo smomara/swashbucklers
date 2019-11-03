85b90f4c57736a87d49edf514fac870efde073b8
Carter Costic <cartercostic@gmail.com>
2019-11-02 23:53:13 -0400

    Quick Windows ps updates

--- Windows/Experimental Scripts/mostRecent.ps1
+++ Windows/Experimental Scripts/mostRecent.ps1
@@ -1,17 +1,26 @@
-if (-not (Test-Path ./recentList.txt) ) {
-    New-Item -Path . -ItemType File -Name "recentList.txt" > $null  # Creates file for output
-}
-if (-not (Test-Path ./recentAnalysis.txt) ) {
-    New-Item -Path . -ItemType File -Name "recentAnalysis.txt" > $null # Creates file for output
-}
+# Prompts the user for the number of files they want outputted at the end
+$numberOfFiles = Read-Host -Prompt "How many files do you want outputted?"
 
+# Makes sure the two files do not exist and creates them
+if (Test-Path ./recentList.txt) {
+    Remove-Item -Path ./recentList.txt
+}
+New-Item -Path . -ItemType File -Name "recentList.txt" > $null  # Creates file for output
+if (Test-Path ./recentAnalysis.txt) {
+    Remove-Item -Path ./recentAnalysis.txt
+}
+New-Item -Path . -ItemType File -Name "recentAnalysis.txt" > $null # Creates file for output
+
+# Searches the C directory for the most recently written to files
 $mostRecent = Get-ChildItem "C:\" -Recurse -File -ErrorAction SilentlyContinue | 
-    Select-Object -Property Name, LastWriteTime, Directory |
+    Select-Object -Property Name, LastWriteTime, Directory |  # Grabs the name, time, and location
     Sort-Object -Descending LastWriteTime |
-    Select-Object -first 200
+    Select-Object -first $numberOfFiles
 
-
+# Outputs the top files to recentList.txt
 $mostRecent | Format-Table Name, LastWriteTime, Directory -AutoSize | Out-File -FilePath ./recentList.txt -Width 4096
+
+# Checks the most recent files for these words, signals if they contain them
 $words = @("password", "secret", "bad", "data", "private")
 
 foreach ($file in $mostRecent) {
