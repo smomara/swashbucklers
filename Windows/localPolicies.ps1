b4368d804e09f35d4c1b10d7f431dd254a4d8ddf
Carter Costic <cartercostic@gmail.com>
2019-11-16 00:20:38 -0500

    Final commit before R2

--- Windows/localPolicies.ps1
+++ Windows/localPolicies.ps1
@@ -6,14 +6,13 @@ Push-Location $OldPath
 
 Write-Host "Copying LGPO"
 if (Test-Path -Path C:\Windows\System32\LGPO) {
-    Remove-Item -Path C:\Windows\System32\LGPO -Recurse
+    Remove-Item -Path C:\Windows\System32\LGPO -Recurse -Force
 }
 Copy-Item -Path .\Win10GPOTemplates\LGPO -Destination C:\Windows\System32 -Recurse
 # Maybe add to Path env var
 
-Write-Host "Applying the configurations."
-# Invoke-Command -ScriptBlock {"LGPO /g .\Win10GPOTemplates\FinishedWin10ENT\{E0EBFD8A-0E3D-4561-B427-10794DEB23C5}"}
-Import-GPO -BackupId E0EBFD8A-0E3D-4561-B427-10794DEB23C5 -Path ".\Win10GPOTemplates\FinishedWin10ENT\{E0EBFD8A-0E3D-4561-B427-10794DEB23C5}"
+Write-Host "Applying configurations"
+LGPO /g '.\Win10GPOTemplates\FinishedWin10ENT\{E0EBFD8A-0E3D-4561-B427-10794DEB23C5}'
 
-Write-Host "Script complete."
-Read-Host "Press any character to exit the script"
+Write-Host "`n`nScript complete."
+Read-Host "Press enter to exit the script"
